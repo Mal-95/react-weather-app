9328b2f383f06d1a9b3f3c6e2055723aa61ca3e9
Mal-95 <malebo445.ml@gmail.com>
2024-06-03 16:02:36 +0200

    Added content to js and css. And dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Weather from './Weather';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Johannesburg" />
+        <footer>This project was coded by{" "}
+        <a href="https://github.com/Mal-95" target="_blank" rel="noreferrer">Malebo Letsoalo,</a>
+        {" "}open-sourced on{" "}
+        <a href="https://github.com/Mal-95/react-weather-project" target="_blank" rel="noreferrer">GitHub</a>
+        {" "}and hosted on{" "}
+        <a href="https://react-weather-app-project-2024.netlify.app" target="_blank" rel="noreferrer">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
